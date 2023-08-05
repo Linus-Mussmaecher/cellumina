631b6318470a8215bb5d7a50930b21da57b952fd
Linus-Mussmaecher <linus@mussmaecher.de>
2023-08-06 01:06:53 +0200

    Added loading from file

--- src/state.rs
+++ src/state.rs
@@ -138,7 +138,9 @@ impl State {
         // +-------------------------------------------------------------+
 
         // Create the state of cells
-        let (cell_state, cells_bind_group_layout) = CellState::new(&device);
+        let (cell_state, cells_bind_group_layout) = 
+        //CellState::new_empty(&device, 100, 50);
+        CellState::new_from_file(&device, "./src/gol_init.txt");
 
         // do a one-time-write
         cell_state.write(&queue);
