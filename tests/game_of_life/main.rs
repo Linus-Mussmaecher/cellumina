c9f3b30759f32d778ec9057f01c59114b6be7eb6
Linus-Mussmaecher <linus.mussmaecher@gmail.com>
2023-08-13 22:43:57 +0200

    Updated readme and added extensive comments to the examples.

--- tests/game_of_life/main.rs
+++ tests/game_of_life/main.rs
@@ -1,32 +1,44 @@
+/// This example implements John Conway's Game Of Life.
 fn main() {
+    // Create a new Cellular Automaton using the builder.
     cellumina::AutomatonBuilder::new()
-        .from_image_buffer(
-            image::io::Reader::open("./tests/game_of_life/gol_init3.png")
-                .expect("Could not read file.")
-                .decode()
-                .expect("Could not decode file.")
-                .into_rgba8(),
-        )
+        // Use an image to supply the initial configuration.
+        .from_image_file("./tests/game_of_life/gol_init3.png")
+        // Describe the rule of Conway's Game Of Life.
         .with_rule(cellumina::rule::EnvironmentRule {
+            // Each cell only cares about neighbors 1 field away.
             range_vert: 1,
             range_hor: 1,
             cell_transform: |env| match env
+            // Iterate over neighbors.
                 .iter()
                 .enumerate()
                 .map(|val| match val {
+                    // The cell we are transforming does not get counted.
                     (4, 'X') => 0,
+                    // Any cell containing an 'X' counts for 1 (alive).
                     (_, 'X') => 1,
+                    // Any cell containing any other entry (only ' ' in our initial configuration) counts as 0 (dead).
                     _ => 0,
                 })
+                // Sum over these 9 values...
                 .sum()
+                // ... and map the sum to the new enty of our cell:
             {
+                // 2 neighbors: The cell keeps its state.
                 2 => env[1][1],
+                // 3 neighbors: The cell gets born.
                 3 => 'X',
+                // 0, 1 or more than 3 neighbors: The cell dies.
                 _ => ' ',
             },
         })
+        // Set a minimum time step.
         .with_min_time_step(std::time::Duration::from_secs_f32(0.2))
+        // Set a display color for the live cells. This color needs to match the color of the live cells in our source image.
         .with_color('X', [106, 190, 48, 255])
+        // Finish the build process.
         .build()
+        // ... and run the automaton with graphical output.
         .run_live();
 }
--- tests/sand/main.rs
+++ tests/sand/main.rs
@@ -2,10 +2,15 @@ use std::collections::HashMap;
 
 use cellumina::rule::Pattern;
 
+/// This example implements a falling-sand-simulation, and also features some other interactions.
 fn main() {
+    // Build an Automaton using the dedicated Builder struct.
     cellumina::AutomatonBuilder::new()
+        // Use a text file as source of initial state.
         .from_text_file("./tests/sand/sand_init.txt")
+        // Now specify the patters we want to use to transform our state.
         .with_patterns(&vec![
+            // Sand (X) falls down by one or even two spaces if possible.
             Pattern {
                 before: grid::grid![['X'][' '][' ']],
                 after: grid::grid![[' '][' ']['X']],
@@ -18,62 +23,85 @@ fn main() {
                 priority: 0.5,
                 ..Default::default()
             },
+            // Stacks of sand collapse to the left or right. The shuffling of rules makes sure this does happen with equal probability.
+            // Note the use of wildcards '*' in the 'after'-grids - these indicate to the automaton that the pattern does not mutate these cells.
             Pattern {
                 before: grid::grid![['X', ' ']['X', ' ']],
-                after: grid::grid![[' ', ' ']['X', 'X']],
+                after: grid::grid![[' ', '*']['X', 'X']],
                 ..Default::default()
             },
             Pattern {
                 before: grid::grid![[' ', 'X'][' ', 'X']],
-                after: grid::grid![[' ', ' ']['X', 'X']],
+                after: grid::grid![['*', ' ']['X', 'X']],
                 ..Default::default()
             },
+            // Even 45 degree slopes of sand collapse (once again to both sides).
             Pattern {
                 before: grid::grid![['X', ' ', ' ']['X', 'X', ' ']],
-                after: grid::grid![[' ', ' ', ' ']['X', 'X', 'X']],
+                after: grid::grid![[' ', '*', '*']['*', '*', 'X']],
                 ..Default::default()
             },
             Pattern {
                 before: grid::grid![[' ', ' ', 'X'][' ', 'X', 'X']],
-                after: grid::grid![[' ', ' ', ' ']['X', 'X', 'X']],
+                after: grid::grid![['*', '*', ' ']['X', '*', '*']],
                 ..Default::default()
             },
+            // Fire has a small chance to fly upwards...
             Pattern {
                 chance: 0.3,
                 before: grid::grid![[' ']['F']],
                 after: grid::grid![['F'][' ']],
                 ..Default::default()
             },
+            // ... and an even smaller chance to fall downwards.
             Pattern {
                 chance: 0.1,
                 before: grid::grid![['F'][' ']],
                 after: grid::grid![[' ']['F']],
                 ..Default::default()
             },
+            // Also, fire can rarely move to the sides. All in all, this creates an upwards-trending random walk.
+            Pattern {
+                chance: 0.1,
+                before: grid::grid![[' ', 'F']],
+                after: grid::grid![['F', ' ']],
+                ..Default::default()
+            },
+            Pattern {
+                chance: 0.1,
+                before: grid::grid![['F', ' ']],
+                after: grid::grid![[' ', 'F']],
+                ..Default::default()
+            },
+            // Fire above, below or next to sand ignites the sand.
             Pattern {
                 chance: 0.8,
                 before: grid::grid![['X']['F']],
-                after: grid::grid![['F']['F']],
-                ..Default::default()
-            },
-            Pattern {
-                chance: 0.8,
-                before: grid::grid![['X', 'F']],
-                after: grid::grid![['F', 'F']],
-                ..Default::default()
-            },
-            Pattern {
-                chance: 0.8,
-                before: grid::grid![['F', 'X']],
-                after: grid::grid![['F', 'F']],
+                after: grid::grid![['F']['*']],
                 ..Default::default()
             },
             Pattern {
                 chance: 0.8,
                 before: grid::grid![['F']['X']],
-                after: grid::grid![['F']['F']],
+                after: grid::grid![['*']['F']],
                 ..Default::default()
             },
+            Pattern {
+                chance: 0.8,
+                before: grid::grid![['X', 'F']],
+                after: grid::grid![['F', '*']],
+                ..Default::default()
+            },
+            Pattern {
+                chance: 0.8,
+                before: grid::grid![['F', 'X']],
+                after: grid::grid![['*', 'F']],
+                ..Default::default()
+            },
+            // Fire can also ignite over corners - this requires another 4 rules.
+            // This could also be solved by using an environment rule!
+            // Note the use of the wildcard in the 'before' grid, as the pattern we are searching for is not rectangular and the contents of these cells do not matter to us.
+            // The repeated wildcard pattern in the 'after' grid then ensures we also do not mutate these cells.
             Pattern {
                 chance: 0.8,
                 before: grid::grid![['X', '*']['*', 'F']],
@@ -98,17 +126,20 @@ fn main() {
                 after: grid::grid![['*', '*']['*', 'F']],
                 ..Default::default()
             },
+            // Fire has a very small chance to decay to ash.
             Pattern {
                 chance: 0.03,
                 before: grid::grid![['F']],
                 after: grid::grid![['A']],
                 priority: 1.,
             },
+            // Ash falls downwards at a slower pace than sand, no 2-move rule here.
             Pattern {
                 before: grid::grid![['A'][' ']],
                 after: grid::grid![[' ']['A']],
                 ..Default::default()
             },
+            // Just like sand, Ash collapses when stacked.
             Pattern {
                 before: grid::grid![['A', ' ']['A', ' ']],
                 after: grid::grid![[' ', '*']['A', 'A']],
@@ -119,14 +150,16 @@ fn main() {
                 after: grid::grid![['*', ' ']['A', 'A']],
                 ..Default::default()
             },
+            // Fire does not ignite Ash, but passes cleanly through it and upwards
             Pattern {
                 before: grid::grid![['A']['F']],
                 after: grid::grid![['F']['A']],
                 ..Default::default()
             },
+            // Ash, just like, fire, can ignite sand, but just from the 4 main directions.
             Pattern {
                 before: grid::grid![['A']['X']],
-                after: grid::grid![[' ']['F']],
+                after: grid::grid![['*']['F']],
                 ..Default::default()
             },
             Pattern {
@@ -135,17 +168,16 @@ fn main() {
                 ..Default::default()
             },
             Pattern {
-                chance: 0.1,
-                before: grid::grid![[' ', 'F']],
-                after: grid::grid![['F', ' ']],
+                before: grid::grid![['A', 'X']],
+                after: grid::grid![['*', 'F']],
                 ..Default::default()
             },
             Pattern {
-                chance: 0.1,
-                before: grid::grid![['F', ' ']],
-                after: grid::grid![[' ', 'F']],
+                before: grid::grid![['X', 'A']],
+                after: grid::grid![['F', '*']],
                 ..Default::default()
             },
+            // Lastly, the Source has a 50% chance of spawning a fire cell above it every time step.
             Pattern {
                 chance: 0.5,
                 before: grid::grid![['*']['S']],
