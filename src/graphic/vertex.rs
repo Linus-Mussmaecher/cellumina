56dd091ee30eaed436e7e23f3e226806c7e98ec5
Linus-Mussmaecher <linus.mussmaecher@gmail.com>
2023-08-13 19:03:04 +0200

    Added comments to rules and display.

--- src/graphic/vertex.rs
+++ src/graphic/vertex.rs
@@ -1,3 +1,4 @@
+/// The number of vertices used in this program.
 pub(super) const VERTICES_COUNT: usize = 4;
 
 /// Vertices forming the corners of a rectangle
@@ -23,17 +24,22 @@ pub(super) const VERTICES: [Vertex; VERTICES_COUNT] = [
 // indices to draw this rectangle of two triangles.
 pub(super) const INDICES: &[u16] = &[0, 1, 2, 1, 3, 2];
 
+/// A vertex in a vertex shader.
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 pub(super) struct Vertex {
+    /// The vertex position in screen coordinates.
     pub(super) position: [f32; 3],
+    /// The coordinates on the texture corresponding to this vertex.
     pub(super) tex_coords: [f32; 2],
 }
 
 impl Vertex {
+    /// A representation of this struct's layout.
     pub(super) const ATTRIBS: [wgpu::VertexAttribute; 2] =
         wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x2];
 
+    /// A representation of a buffer containing multiple vertices.
     pub(super) fn desc() -> wgpu::VertexBufferLayout<'static> {
         wgpu::VertexBufferLayout {
             // how far are two elements in the buffer from each other
