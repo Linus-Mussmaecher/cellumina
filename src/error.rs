e53afe11c7b828b35e9f1b2c798fe89c9c401f99
Linus-Mussmaecher <linus.mussmaecher@gmail.com>
2023-08-27 01:29:26 +0200

    Improved error handling with thiserror.

--- src/error.rs
+++ src/error.rs
@@ -1,41 +1,18 @@
+use thiserror::Error;
+
 /// Internal Cellumina library error type.
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum CelluminaError {
-    /// An Index-Out-Of-Bounds-Error when accessing the underlying state grid of an automaton.
+    /// Any different type of error.
+    #[error("{0}")]
     CustomError(String),
+    /// An Index-Out-Of-Bounds-Error when accessing the underlying state grid of an automaton.
+    #[error("index ({0}, {1}) out of bounds for state grid of size ({2}, {3})")]
     IndexOutOfBoundsError(u32, u32, u32, u32),
-    IOError(std::io::Error),
-    ImageError(image::error::ImageError),
-}
-
-impl std::fmt::Display for CelluminaError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            CelluminaError::IndexOutOfBoundsError(row, col, max_row, max_col) => {
-                write!(
-                    f,
-                    "Index ({row}, {col}) out of bounds for grid of size ({max_row}, {max_col})."
-                )?;
-                Ok(())
-            }
-            Self::CustomError(msg) => {
-                write!(f, "{msg}")?;
-                Ok(())
-            }
-            CelluminaError::IOError(e) => e.fmt(f),
-            CelluminaError::ImageError(e) => e.fmt(f),
-        }
-    }
-}
-
-impl From<std::io::Error> for CelluminaError {
-    fn from(value: std::io::Error) -> Self {
-        Self::IOError(value)
-    }
-}
-
-impl From<image::error::ImageError> for CelluminaError {
-    fn from(value: image::error::ImageError) -> Self {
-        Self::ImageError(value)
-    }
+    /// Error passed on from std::io.
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
+    /// Error passed on from the image crate.
+    #[error(transparent)]
+    ImageError(#[from] image::error::ImageError),
 }
